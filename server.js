31f5c6281b391da9206c98c4bd52b0e078756b81
KevinQuAaxis <kqu@aaxiscommerce.com>
2016-07-28 15:01:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ switch(env) {
     case 'production':
         console.log('*** PROD ***');
         
-        app.use(express.bodyParser()); 
+        //app.use(express.bodyParser()); 
         app.use(express.static(config.root + config.compile.replace('.', '')));
         app.post('/orderSubmit', function(req1, res1) {
             
